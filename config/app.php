7440a5db383f44e5b17cc437a50a0d0491b4ee0a
Igor Telmenko <igor-telmenko@yandex.ru>
2017-07-29 14:54:41 +0300

    Package nwidart/laravel-modules installed

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Laravel Modules
+        Nwidart\Modules\LaravelModulesServiceProvider::class,
+
+
     ],
 
     /*
@@ -225,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Module' => Nwidart\Modules\Facades\Module::class,
 
     ],
 
